a7ded02ef0045b031b2b3d0ae2ba05e98ecc7dcb
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-10-28 16:38:09 -0300

    campos de texto do formulario finalizados

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -1,10 +1,15 @@
 import "./CampoTexto.css";
 
-const CampoTexto = () => {
+const CampoTexto = (props) => {
+
+    const placeholderModificada = `${props.placeholder}...`
+
   return (
     <div className="campo-texto">
-      <label>Nome</label>
-      <input />
+      <label>
+        {props.label}
+        </label>
+      <input placeholder={placeholderModificada} />
     </div>
   );
 };
