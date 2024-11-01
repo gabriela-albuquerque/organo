f944d9aab9e4ab909e0759ecfe6b11c2d24bf078
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-11-01 18:44:41 -0300

    formulario envia ao App o objeto contendo as informacoes digitadas pelo usuario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Banner from './componentes/Banner';
-import Formulario from './componentes/Formulario';
+import { useState } from "react";
+import Banner from "./componentes/Banner";
+import Formulario from "./componentes/Formulario";
 
 function App() {
+  const [colaborador, setColaboradores] = useState([]);
+
+  const AoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador);
+  };
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />          
+      <Formulario
+        aoColaboradorCadastrado={(colaborador) =>
+          AoNovoColaboradorAdicionado(colaborador)
+        }
+      />
     </div>
   );
 }
