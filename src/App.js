d895ed49c6753f94fbac0261779605d1132244a2
Gabriela Albuquerque <126294239+gabriela-albuquerque@users.noreply.github.com>
2024-11-04 09:34:06 -0300

    implementação do componente rodape

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Banner from "./componentes/Banner";
 import Formulario from "./componentes/Formulario";
 import Time from "./componentes/Time";
+import Rodape from "./componentes/Rodape";
 
 function App() {
   const times = [
@@ -64,6 +65,7 @@ function App() {
           )}
         />
       ))}
+      <Rodape />
     </div>
   );
 }
